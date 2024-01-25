ebdbfb2dd9ee4f7992914dc6a63a3075a2f3b918
Kirby Manjarres <KManjarres@luc.edu>
2024-01-25 07:27:41 +0000

    added Kirby_WarmUp_02

---
+++ WarmUp_02/Calculator.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    double num1, num2;
+
+    printf("Enter first number: ");
+    scanf("%lf", &num1);
+
+    printf("Enter second number: ");
+    scanf("%lf", &num2);
+
+    printf("Sum: %.2f\n", num1 + num2);
+    printf("Difference: %.2f\n", num1 - num2);
+    printf("Product: %.2f\n", num1 * num2);
+
+    if (num2 != 0) {
+        printf("Quotient: %.2f\n", num1 / num2);
+    } else {
+        printf("Cannot divide by zero.\n");
+    }
+
+    return 0;
+}
